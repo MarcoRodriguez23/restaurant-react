a5efc799a4c436421c1852b659193cc36f6f1f0c
Marco Rodriguez <rodriguezlmarco23@gmail.com>
2023-02-22 21:47:55 -0600

    Create login form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,52 @@
 export default function login() {
   return (
-    <div>login</div>
+    <>
+      <h1 className="text-4xl font-black">Iniciar sesión</h1>
+      <p>Para crear un pedido, debes iniciar sesión.</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+          <div className="mb-4">
+            <label 
+              htmlFor="email"
+              className="text-slate-800"
+            >
+              Email:
+            </label>
+            <input 
+              id="email"
+              type="email" 
+              className="mt-2 w-full p-3 bg-gray-50 border border-slate-200 rounded-md"
+              name="email"
+              placeholder="Tu email"
+              
+            />
+          </div>
+
+          <div className="mb-4">
+            <label 
+              htmlFor="password"
+              className="text-slate-800"
+            >
+              Password:
+            </label>
+            <input 
+              id="password"
+              type="password" 
+              className="mt-2 w-full p-3 bg-gray-50 border border-slate-200 rounded-md"
+              name="password"
+              placeholder="Tu password"
+              
+            />
+          </div>
+
+          <input 
+            type="submit" 
+            value="Iniciar sesión" 
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer rounded-md" 
+          />
+        </form>
+      </div>
+    </>
   )
 }
